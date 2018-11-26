d27c312000857a6695125c9d1f822a073a57f520
David Saltares <david.saltares@gmail.com>
2018-11-26 11:19:33 +0000

    Add example of running tests on Godot on CI
    Specify local godot file
    
    Execute permissions on godot
    
    sligly better readme
    
    Fix tests
    
    Test downloading godot on build
    
    Use separate run_tests.sh script

---
+++ project/test/unit/test_example.gd
@@ -0,0 +1,19 @@
+extends "res://addons/gut/test.gd"
+func before_each():
+	gut.p("ran setup", 2)
+
+func after_each():
+	gut.p("ran teardown", 2)
+
+func before_all():
+	gut.p("ran run setup", 2)
+
+func after_all():
+	gut.p("ran run teardown", 2)
+
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
+
+func test_assert_true_with_true():
+	assert_true(true, "Should pass, true is true")
+
